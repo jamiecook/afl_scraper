30e585b28c00d8fab7134b9f1910c2e0300d4104
Jamie Cook <jamie.cook@veitchlister.com.au>
2015-03-26 22:51:09 +1000

    actually do stuff

--- scrape.rb
+++ scrape.rb
@@ -11,33 +11,41 @@ Score = Struct.new(:goals, :behinds) do
     goals * 6 + behinds
   end
 end
-GameScore = Struct.new(:q1_score, :q2_score, :q3_score, :q4_score) do 
+GameScore = Struct.new(:team, :q1_score, :q2_score, :q3_score, :q4_score) do
   def total
-    [q1_score, q2_score, q3_score, q4_score].sum(&:total)
+    q4_score.total
   end
 end
 LadderEntry = Struct.new(:team, :wins, :points, :ratio)
 # Ladder = [LadderEntry]
 
-def parse_results(results) 
+def parse_results(results)
   # do something
 end
 
-def parse_ladder(ladder) 
+def parse_ladder(ladder)
   # do something
 end
 
-page = Nokogiri::HTML(open("2014.html"))   
-puts page.class   # => Nokogiri:HTML::Document
+def parse_team_score(name, rounds, total)
+  GameScore.new(name, *rounds.scan(/(\d+)\.(\d+)/).map { |g,b| Score.new(g.to_i, b.to_i) })
+end
 
+def parse_result(result)
+  cells = result.css('td')
+  t1 = parse_team_score(*cells[0..2].map(&:text))
+  t2 = parse_team_score(*cells[4..6].map(&:text))
+  Result.new(t1,t2)
+end
 
+page = Nokogiri::HTML(open("2014.html"))
 
-results = page.xpath('//td[table]') # 46 of these
-results.each_cons(2).first(1).map { |results,ladder|
-  result = parse_results(results)
-  ladder = parse_ladder(ladder)
-  p results.class
-  p results.css('tr td').size
-  
+puts page.class   # => Nokogirij:HTML::Document
+# puts page.css('table#root').map { |t| t['width'] }.size
+results = page.xpath('//td[table]')
+results.each_cons(2).first(1).map { |round,ladder|
+  results = round.children.select { |e| e.name == 'table' }.first(1).map { |e| parse_result(e) }
+  # result = parse_results(results)
+  # ladder = parse_ladder(ladder)
 }
 
